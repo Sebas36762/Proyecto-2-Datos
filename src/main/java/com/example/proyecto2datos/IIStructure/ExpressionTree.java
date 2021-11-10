c9f002d979a4ccd6028ffe812b4a5725148edd5c
Sebas36762 <84096977+Sebas36762@users.noreply.github.com>
2021-11-10 15:34:03 -0600

    Arreglo Arbol Printeo de resultados

--- src/main/java/com/example/proyecto2datos/IIStructure/ExpressionTree.java
+++ src/main/java/com/example/proyecto2datos/IIStructure/ExpressionTree.java
@@ -31,7 +31,7 @@ public class ExpressionTree {
     }
 
 
-    Node constructTree(char postfix[]) {
+    public Node constructTree(char postfix[]) {
         /*
         Entrada Char list
         Salida Node
@@ -127,7 +127,94 @@ public class ExpressionTree {
         return et.calculator(root);
 
     }
+    public static int precedence(char ch)
+    {
+        /*
+        Entrada Char
+        Salida Entero
+        Este metodo indetifica si el char es un operador.
+         */
+        if(ch=='+' || ch=='-')
+            return 1;
 
+        else if(ch=='*' || ch=='/')
+            return 2;
+
+        return 0;
+    }
+    public static String convertToPostfix(String exp)
+    { /*
+         Entrada String, la expresión infija
+         Salida String,  la expresión postfija
+         Este metodo convierte el string (expresión) de fija a postfija.
+              */
+
+        Stack<Character> operators = new Stack<>();
+        Stack<String> postFix = new Stack<>();
+
+        for(int i=0;i<exp.length();i++)
+        {
+            char ch=exp.charAt(i);         // current character.
+
+            if(ch=='(')
+                operators.push(ch);
+
+            else if((ch>='0' && ch<='9') || (ch>='A' && ch<='Z'))
+                postFix.push(ch+"");
+
+            else if(ch==')')
+            {
+                while(operators.peek()!= '(')
+                {
+                    // STEP 5 of Algorithm.
+                    char op = operators.pop();
+
+                    String first = postFix.pop();          // get the two operands.
+                    String second = postFix.pop();
+
+                    String new_postFix = second+first+op;
+
+                    postFix.push(new_postFix);
+                }
+
+                operators.pop();
+            }
+
+
+            else if(ch=='+' || ch=='-' || ch== '*' || ch== '/')
+            {
+
+                while(operators.size()>0 && operators.peek()!='(' && precedence(ch) <= precedence(operators.peek()))
+                {
+                    char op = operators.pop();
+
+                    String first = postFix.pop();
+                    String second = postFix.pop();
+
+                    String new_postFix = second+first+op;
+
+                    postFix.push(new_postFix);
+                }
+
+                operators.push(ch);
+            }
+        }
+
+
+        while(operators.size()>0)
+        {
+            char op = operators.pop();
+
+            String first = postFix.pop();
+            String second = postFix.pop();
+
+            String new_postFix = second+first+op;
+
+            postFix.push(new_postFix);
+        }
+
+        return postFix.pop();
+    }
 
 }
 
--- src/main/webapp/procesar.jsp
+++ src/main/webapp/procesar.jsp
@@ -1,4 +1,6 @@
 <%@ page import="com.example.proyecto2datos.IIStructure.ExpressionTree" %>
+<%@ page import="com.example.proyecto2datos.IIStructure.Postfix" %>
+<%@ page import="com.example.proyecto2datos.IIStructure.Node" %>
 <%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
 
 <!DOCTYPE html>
@@ -7,13 +9,24 @@
     <title>BinaryTree</title>
     <link href="css/estilos.css" rel="stylesheet" type="text/css"/>
     <%
-        String Value ="";
-        if(request.getParameter("BotonEnv")!=null){
+        String Value = "";
+        String resultado;
+        if (request.getParameter("BotonEnv") != null) {
             Value = request.getParameter("txtdatos");
-        }else
+            String ex = request.getParameter("txtdatos");
+            System.out.println(ex);
+            ExpressionTree et = new ExpressionTree();
+            Postfix postfix = new Postfix();
+            Node root = et.constructTree(postfix.convertToPostfix(ex).toCharArray());
+            resultado = String.valueOf(et.calculator(root));
+            System.out.println(resultado);
+        } else
             return;
 
+
     %>
+
+
 
 
 </head>
@@ -21,11 +34,16 @@
 <body>
 <div id="Cuadro">
     <h4>
-            <%=Value%>
+        <%=Value%>
+        <br></br>
+        El resultado de la expresión ingresada es:
+
+        <%=resultado%>
+
 
     </h4>
 
-    <%= ExpressionTree.main("")%>
+
 
 
 
